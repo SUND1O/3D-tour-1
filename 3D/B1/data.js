e08e6ba2264230d7fe0597523fd3278f86554df6
Wiktor P <85827318+SUND1O@users.noreply.github.com>
2025-04-16 11:27:20 +0200

    work ffs

---
+++ 3D/B1/data.js
@@ -0,0 +1,309 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-korytarz",
+      "name": "Korytarz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.07786853585821163,
+        "pitch": 0.3645978151752889,
+        "fov": 1.3185972797531151
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.742232529094542,
+          "pitch": 0.4431287566340334,
+          "rotation": 0,
+          "target": "4-azienka"
+        },
+        {
+          "yaw": 0.11215766403707583,
+          "pitch": 0.3502579630569329,
+          "rotation": 0,
+          "target": "5-pokj"
+        },
+        {
+          "yaw": 2.4264782100982387,
+          "pitch": 0.5247833556735255,
+          "rotation": 0,
+          "target": "1-kuchnia-1"
+        },
+        {
+          "yaw": 0.6255008102067023,
+          "pitch": 0.3314583205019499,
+          "rotation": 0,
+          "target": "6-sypialnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kuchnia-1",
+      "name": "Kuchnia 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.029536341187597515,
+        "pitch": 0.296589932551921,
+        "fov": 1.3185972797531151
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.520262134064641,
+          "pitch": 0.28312603918651824,
+          "rotation": 0,
+          "target": "3-balkon"
+        },
+        {
+          "yaw": -2.7929934862100136,
+          "pitch": 0.5559451067331427,
+          "rotation": 0,
+          "target": "0-korytarz"
+        },
+        {
+          "yaw": -2.6721012108333237,
+          "pitch": 0.29666980172559754,
+          "rotation": 0,
+          "target": "4-azienka"
+        },
+        {
+          "yaw": -0.269613054771332,
+          "pitch": 0.35412149387339475,
+          "rotation": 0,
+          "target": "2-kuchnia-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kuchnia-2",
+      "name": "Kuchnia 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.38482576961825643,
+        "pitch": 0.3847523679856941,
+        "fov": 1.3185972797531151
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.500487779647184,
+          "pitch": 0.4818987137002697,
+          "rotation": 0,
+          "target": "3-balkon"
+        },
+        {
+          "yaw": -0.7898273173596557,
+          "pitch": 0.30782116350130906,
+          "rotation": 0,
+          "target": "1-kuchnia-1"
+        },
+        {
+          "yaw": -0.5348186800636476,
+          "pitch": 0.27927835043179705,
+          "rotation": 0,
+          "target": "0-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-balkon",
+      "name": "Balkon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4556411568017147,
+          "pitch": 0.681959360059814,
+          "rotation": 0,
+          "target": "1-kuchnia-1"
+        },
+        {
+          "yaw": -1.723639162477273,
+          "pitch": 0.5247856365365173,
+          "rotation": 0,
+          "target": "2-kuchnia-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.2426482821180933,
+        "pitch": 0.0513752422685414,
+        "fov": 1.3185972797531151
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4433394200867937,
+          "pitch": 0.6017909161881612,
+          "rotation": 0,
+          "target": "0-korytarz"
+        },
+        {
+          "yaw": -2.3312028361135386,
+          "pitch": 0.33286242091515916,
+          "rotation": 0,
+          "target": "1-kuchnia-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pokj",
+      "name": "Pokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.15645406379582383,
+        "pitch": 0.3941094213277907,
+        "fov": 1.3185972797531151
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.502620369652414,
+          "pitch": 0.361440856357099,
+          "rotation": 0,
+          "target": "0-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sypialnia",
+      "name": "Sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.5677762241701991,
+        "pitch": 0.2774070380242417,
+        "fov": 1.3185972797531151
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.213054342270258,
+          "pitch": 0.4200692131488051,
+          "rotation": 0,
+          "target": "0-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Spacer 3D 2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
