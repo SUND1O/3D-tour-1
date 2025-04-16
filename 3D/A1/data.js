3db534f9d8a4b9dac7826a4cbaed0ba0b57c278a
Wiktor P <85827318+SUND1O@users.noreply.github.com>
2025-04-16 12:31:40 +0200

    Add files via upload

--- 3D/A1/data.js
+++ 3D/A1/data.js
@@ -12,9 +12,13 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
         }
       ],
-      "faceSize": 480,
+      "faceSize": 1024,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -22,22 +26,120 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.2694615400618172,
-          "pitch": 0.30844708010323174,
+          "yaw": -0.35830720808371197,
+          "pitch": 0.5643127818008864,
           "rotation": 0,
-          "target": "1-okno"
+          "target": "3-okno"
         },
         {
-          "yaw": 0.31822004909442825,
-          "pitch": 0.02427739314766697,
+          "yaw": 1.0262025441022065,
+          "pitch": 0.7161676948462627,
           "rotation": 0,
-          "target": "2-drabina"
+          "target": "2-d"
+        },
+        {
+          "yaw": 1.0031938588689648,
+          "pitch": -0.39622103096310823,
+          "rotation": 0,
+          "target": "1-gra"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-okno",
+      "id": "1-gra",
+      "name": "Góra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.076945682229015,
+          "pitch": 1.0439227812393703,
+          "rotation": 0,
+          "target": "3-okno"
+        },
+        {
+          "yaw": 1.3694488340800355,
+          "pitch": 0.888346354402163,
+          "rotation": 0,
+          "target": "0-wejcie"
+        },
+        {
+          "yaw": 0.6058396189954571,
+          "pitch": 0.8043791082040688,
+          "rotation": 0,
+          "target": "2-d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-d",
+      "name": "Dół",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3557990164110088,
+          "pitch": 0.39801556004735517,
+          "rotation": 0,
+          "target": "0-wejcie"
+        },
+        {
+          "yaw": -0.04276200028934518,
+          "pitch": 0.32248780523606513,
+          "rotation": 0,
+          "target": "3-okno"
+        },
+        {
+          "yaw": 2.4953787538638457,
+          "pitch": -1.5035203170589906,
+          "rotation": 0,
+          "target": "1-gra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-okno",
       "name": "Okno",
       "levels": [
         {
@@ -48,45 +150,13 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 512
-        }
-      ],
-      "faceSize": 480,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.3650341564518058,
-          "pitch": 0.2969215153310003,
-          "rotation": 0,
-          "target": "0-wejcie"
-        },
-        {
-          "yaw": -0.01110432893296398,
-          "pitch": -0.12605285996411197,
-          "rotation": 0,
-          "target": "2-drabina"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-drabina",
-      "name": "Drabina",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
         },
         {
           "tileSize": 512,
-          "size": 512
+          "size": 1024
         }
       ],
-      "faceSize": 480,
+      "faceSize": 1024,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -94,16 +164,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.6388822832868186,
-          "pitch": 0.6127858822965919,
+          "yaw": -2.968512218189268,
+          "pitch": 0.5790806760917491,
           "rotation": 0,
           "target": "0-wejcie"
         },
         {
-          "yaw": -2.2205970525069514,
-          "pitch": 1.1296845799366668,
+          "yaw": 2.270034189000471,
+          "pitch": 0.6198849493025804,
           "rotation": 0,
-          "target": "1-okno"
+          "target": "2-d"
+        },
+        {
+          "yaw": 2.3495401670124902,
+          "pitch": -0.29476510703119274,
+          "rotation": 0,
+          "target": "1-gra"
         }
       ],
       "infoHotspots": []
